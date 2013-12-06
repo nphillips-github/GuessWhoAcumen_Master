3d68f03ff5cb78c50b7bdda20ef5dbe627056b39
devmhighwind <mchin@acumensolutions.com>
2013-12-06 13:40:58 -0500

    Adding A data object

---
+++ GuessWhoAcumen/src/net/nrp/datamodels/Person.java
@@ -0,0 +1,11 @@
+package net.nrp.datamodels;
+
+/**
+ * Created by mchin on 12/5/13.
+ */
+public class Person {
+    public String fullPhotoUrl;
+    public String iD;
+    public String firstName;
+    public String lastName;
+}
